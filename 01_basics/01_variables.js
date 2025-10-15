ae36b791d205b43764a1ab9ddca4c20271d304e4
baljit24 <101459481+baljit24@users.noreply.github.com>
2025-10-15 07:03:06 +0000

    data__types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "baljit@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountstate;
+
+accountEmail = "123@gmail.com"
+accountPassword = "1552"
+accountCity = "bengaluru"
+// accountId = 2
+
+// console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountstate])

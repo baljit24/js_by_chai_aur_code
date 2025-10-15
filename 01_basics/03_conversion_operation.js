8ffc0b208eb3c5159466f26dfa85f1c0e9fb4630
baljit24 <101459481+baljit24@users.noreply.github.com>
2025-10-15 07:28:14 +0000

    conversion_of_data_type

---
+++ 01_basics/03_conversion_operation.js
@@ -0,0 +1,20 @@
+// let score = "33" 
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+let isLoginIn = 1
+let booleanIsLogin = Boolean(isLoginIn)
+console.log(isLoginIn);
+//  in boolean 1 = true, 0 = false
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)

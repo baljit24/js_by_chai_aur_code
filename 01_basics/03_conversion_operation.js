9d03ada11e95269d078d261260281d14f1b36b79
baljit24 <101459481+baljit24@users.noreply.github.com>
2025-10-15 09:58:09 +0000

    comparision

--- 01_basics/03_conversion_operation.js
+++ 01_basics/03_conversion_operation.js
@@ -8,13 +8,33 @@
 // console.log(valueInNumber)
 
 
-let isLoginIn = 1
-let booleanIsLogin = Boolean(isLoginIn)
-console.log(isLoginIn);
+// let isLoginIn = 1
+// let booleanIsLogin = Boolean(isLoginIn)
+// console.log(isLoginIn);
 //  in boolean 1 = true, 0 = false
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// -------------------------------------
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+
+
+
+// console.log(true);
+// console.log(+true);
+
+// console.log(+"");
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+++gameCounter
+console.log(gameCounter);
